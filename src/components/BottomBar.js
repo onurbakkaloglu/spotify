b328048f28e9c9bcec4f8577fb8461e6c04a3e7d
akifyilmazz <mehmetakif.akifmehmet@outlook.com>
2021-11-16 13:24:01 +0300

    .

---
+++ src/components/BottomBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BottomBar = () => {
+    return (
+        <div className="h-24">
+            bottombar
+        </div>
+    )
+}
+
+export default BottomBar
---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <asside className="w-60">
+            sidebar
+        </asside>
+    )
+}
+
+export default Sidebar
