1c3c1747008f73906205756b532c66e66c00e2aa
akifyilmazz <mehmetakif.akifmehmet@outlook.com>
2021-11-16 13:35:52 +0300

    .

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,7 +5,7 @@ const Content = () => {
     return (
         <main className="flex-auto">
             <Navbar/>
-            contentfhgmj
+            contentfhgmjdfghdfh
         </main>
     )
 }
